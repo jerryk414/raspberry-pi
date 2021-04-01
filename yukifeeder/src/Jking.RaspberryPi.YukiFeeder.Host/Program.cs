a577e57d80fc97965ef17eb20fd4877cf5983677
Jerry <jerryk414@gmail.com>
2021-03-31 21:17:42 -0400

    Initial commit

---
+++ yukifeeder/src/Jking.RaspberryPi.YukiFeeder.Host/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Device.Gpio;
+using System.Device.Gpio.Drivers;
+using System.IO;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jking.RaspberryPi.YukiFeeder.Host
+{
+    class Program
+    {
+        // Command to copy to raspberry pi
+        // scp C:\Users\Jerry\Source\Repos\Jking.RaspberryPi.Console\Jking.RaspberryPi.Host\bin\Debug\netcoreapp3.1\linux-arm\* pi@192.168.1.51:/home/pi/Documents
+        static async Task Main(string[] args)
+        {
+            using (GpioController controller = new GpioController())
+            {
+                controller.OpenPin(_ledPin, PinMode.Output);
+                controller.Write(_ledPin, PinValue.High);
+
+                controller.OpenPin(_buttonPin, PinMode.InputPullDown);
+
+                controller.RegisterCallbackForPinValueChangedEvent(_buttonPin, PinEventTypes.Falling, async (s, e) =>
+                {
+                    await DispenseTreat(controller).ConfigureAwait(false);
+                });
+
+                await using (WebSocketConnection connection = await WebSocketConnection.BeginListen(async s =>
+                {
+                    await DispenseTreat(controller).ConfigureAwait(false);
+                }))
+                {
+                    await Task.Delay(-1);
+                }
+            }
+        }
+
+        private static async Task DispenseTreat(GpioController controller)
+        {
+            if (!_treatBeingDispensed)
+            {
+                _treatBeingDispensed = true;
+
+                controller.Write(_ledPin, PinValue.Low);
+
+                await Task.Delay(1000).ConfigureAwait(false);
+
+                controller.Write(_ledPin, PinValue.High);
+
+                _treatBeingDispensed = false;
+            }
+        }
+
+        private static bool _treatBeingDispensed;
+        private static int _ledPin = 18;
+        private static int _buttonPin = 23;
+    }
+}
---
+++ yukifeeder/src/Jking.RaspberryPi.YukiFeeder.Host/WebSocketConnection.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jking.RaspberryPi.YukiFeeder.Host
+{
+    public class WebSocketConnection : IAsyncDisposable
+    {
+        #region Construction
+
+        private WebSocketConnection(Action<string> onMessageReceived)
+        {
+            _webSocket = new ClientWebSocket();
+            _cts = new CancellationTokenSource();
+            _onMessageReceived = onMessageReceived;
+        }
+
+        #endregion
+
+        #region Fields
+
+        private static Uri _apiUri = new Uri(@"wss://3kfqv32kr2.execute-api.us-east-1.amazonaws.com/production");
+        private static int _bufferSize = 8192;
+
+        private readonly ClientWebSocket _webSocket;
+        private readonly CancellationTokenSource _cts;
+        private readonly Action<string> _onMessageReceived;
+
+        #endregion
+
+        #region Methods
+
+        public static async Task<WebSocketConnection> BeginListen(Action<string> onMessageReceived)
+        {
+            WebSocketConnection connection = new WebSocketConnection(onMessageReceived);
+            await connection.ConnectAsync();
+
+            return connection;
+        }
+
+        public async Task ConnectAsync()
+        {
+            await _webSocket.ConnectAsync(_apiUri, _cts.Token).ConfigureAwait(false);
+
+            Task.Run(async () => await LoopAsync().ConfigureAwait(false));
+        }
+
+        private async Task LoopAsync()
+        {
+            WebSocketReceiveResult receiveResult = null;
+            byte[] buffer = new byte[_bufferSize];
+
+            while (!_cts.Token.IsCancellationRequested)
+            {
+                using (MemoryStream outputStream = new MemoryStream(_bufferSize))
+                {
+                    do
+                    {
+                        receiveResult = await _webSocket.ReceiveAsync(buffer, _cts.Token);
+                        if (receiveResult.MessageType != WebSocketMessageType.Close)
+                            outputStream.Write(buffer, 0, receiveResult.Count);
+                    }
+                    while (!receiveResult.EndOfMessage);
+
+                    if (receiveResult.MessageType == WebSocketMessageType.Close)
+                        break;
+
+                    outputStream.Position = 0;
+
+                    using (StreamReader reader = new StreamReader(outputStream))
+                    {
+                        string message = reader.ReadToEnd();
+
+                        _onMessageReceived(message);
+                    }
+                }
+            }
+        }
+
+        ~WebSocketConnection()
+        {
+            DisposeAsync();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            if (_webSocket != null)
+            {
+                if (_webSocket.State.Equals(WebSocketState.Open))
+                { 
+                    await _webSocket.CloseOutputAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None).ConfigureAwait(false);
+                    //await _webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, "", CancellationToken.None).ConfigureAwait(false);
+                }
+
+                _webSocket.Dispose();
+            }
+
+            _cts?.Dispose();
+
+            _disposed = true;
+        }
+
+        #endregion
+    }
+}
